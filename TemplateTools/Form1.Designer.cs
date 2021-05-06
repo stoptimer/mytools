2c2a674e53ad7d12cf31706e9bb937c6d15fb7ff
xyc309 <xyc309@gmail.com>
2021-05-06 13:54:57 +0800

    first commit

---
+++ TemplateTools/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace TemplateTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_connection = new System.Windows.Forms.TextBox();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_pwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_namespace = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmb_tables = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_selecttables = new System.Windows.Forms.Button();
+            this.tb_lib = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(125, 331);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "生成";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数据库连接";
+            // 
+            // tb_connection
+            // 
+            this.tb_connection.Location = new System.Drawing.Point(177, 51);
+            this.tb_connection.Name = "tb_connection";
+            this.tb_connection.Size = new System.Drawing.Size(240, 23);
+            this.tb_connection.TabIndex = 2;
+            this.tb_connection.Text = "10.100.4.154";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(177, 97);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(240, 23);
+            this.tb_username.TabIndex = 4;
+            this.tb_username.Text = "bpmuser";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "用户名";
+            // 
+            // tb_pwd
+            // 
+            this.tb_pwd.Location = new System.Drawing.Point(177, 144);
+            this.tb_pwd.Name = "tb_pwd";
+            this.tb_pwd.Size = new System.Drawing.Size(240, 23);
+            this.tb_pwd.TabIndex = 6;
+            this.tb_pwd.Text = "123456";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "密码";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "表";
+            // 
+            // tb_namespace
+            // 
+            this.tb_namespace.Location = new System.Drawing.Point(177, 278);
+            this.tb_namespace.Name = "tb_namespace";
+            this.tb_namespace.Size = new System.Drawing.Size(240, 23);
+            this.tb_namespace.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "空间名";
+            // 
+            // cmb_tables
+            // 
+            this.cmb_tables.FormattingEnabled = true;
+            this.cmb_tables.Location = new System.Drawing.Point(176, 233);
+            this.cmb_tables.Name = "cmb_tables";
+            this.cmb_tables.Size = new System.Drawing.Size(240, 25);
+            this.cmb_tables.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "数据库";
+            // 
+            // btn_selecttables
+            // 
+            this.btn_selecttables.Location = new System.Drawing.Point(443, 235);
+            this.btn_selecttables.Name = "btn_selecttables";
+            this.btn_selecttables.Size = new System.Drawing.Size(75, 23);
+            this.btn_selecttables.TabIndex = 14;
+            this.btn_selecttables.Text = "获取表";
+            this.btn_selecttables.UseVisualStyleBackColor = true;
+            this.btn_selecttables.Click += new System.EventHandler(this.btn_selecttables_Click);
+            // 
+            // tb_lib
+            // 
+            this.tb_lib.Location = new System.Drawing.Point(176, 188);
+            this.tb_lib.Name = "tb_lib";
+            this.tb_lib.Size = new System.Drawing.Size(241, 23);
+            this.tb_lib.TabIndex = 15;
+            this.tb_lib.Text = "BPM_LongYa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_lib);
+            this.Controls.Add(this.btn_selecttables);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmb_tables);
+            this.Controls.Add(this.tb_namespace);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_pwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_connection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_connection;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_pwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_namespace;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmb_tables;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_selecttables;
+        private System.Windows.Forms.TextBox tb_lib;
+    }
+}
+
---
+++ TemplateTools/Form1.cs
@@ -0,0 +1,177 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemplateTools
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            string sql = $@"SELECT
+                  COLUMN_NAME name,
+                  DATA_TYPE datatype,
+                  COLUMN_COMMENT remark
+                FROM
+                 INFORMATION_SCHEMA.COLUMNS
+                where
+                table_name = '{this.cmb_tables.SelectedValue}'";
+            IEnumerable<dynamic> tables = null;
+            using (IDbConnection conn = GetConn())
+            {
+                conn.Open();
+                tables = conn.Query<dynamic>(sql);
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"using System;");
+            builder.AppendLine($"using System.Collections.Generic;");
+            builder.AppendLine($"using System.Text;");
+            builder.AppendLine($"namespace {this.tb_namespace.Text}");
+            builder.AppendLine("{");
+            builder.AppendLine($"    public class {this.cmb_tables.SelectedValue} : AggregateRoot");
+            builder.AppendLine("    {");
+            foreach (var item in tables)
+            {
+                if (item.name != "Id")
+                {
+                    builder.AppendLine("        /// <summary>");
+                    builder.AppendLine($"        /// {item.remark}");
+                    builder.AppendLine("        /// <summary>");
+                    builder.AppendLine($"        public {GetDataType(item.datatype)} {item.name} {{ get; set; }}");
+                }
+
+            }
+            builder.AppendLine("    }");
+            builder.AppendLine("}");
+            string path = "D:\\output\\";
+            string fullPath = path + this.cmb_tables.SelectedValue + ".cs";
+            if (!Directory.Exists(path))//如果不存在就创建file文件夹
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            StreamWriter sw = new StreamWriter(fullPath, false, Encoding.Default);
+            sw.WriteLine(builder.ToString());
+            sw.Flush();
+            sw.Close();
+            MessageBox.Show("生成完毕！");
+        }
+        private IDbConnection GetConn()
+        {
+            return new MySqlConnection($"Database={this.tb_lib.Text.Trim()};Data Source={this.tb_connection.Text.Trim()};User Id={this.tb_username.Text.Trim()};Password={this.tb_pwd.Text.Trim()};Connect Timeout=600");
+        }
+        private void btn_selecttables_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> tables = null;
+            using (IDbConnection conn = GetConn())
+            {
+                conn.Open();
+                tables = conn.Query<string>($"select table_name from information_schema.tables where table_schema='{this.tb_lib.Text.Trim()}'");
+            }
+            this.cmb_tables.DataSource = tables;
+        }
+
+        private string GetDataType(string dataType)
+        {
+            string typeString = "object";
+            switch (dataType)
+            {
+                case "int":
+                    typeString = "int";
+                    break;
+                case "text":
+                    typeString = "string";
+                    break;
+                case "bigint":
+                    typeString = "int";
+                    break;
+                case "binary":
+                    typeString = "byte[]";
+                    break;
+                case "bit":
+                    typeString = "bool";
+                    break;
+                case "char":
+                    typeString = "string";
+                    break;
+                case "datetime":
+                    typeString = "DateTime";
+                    break;
+                case "decimal":
+                    typeString = "decimal";
+                    break;
+                case "float":
+                    typeString = "double";
+                    break;
+                case "image":
+                    typeString = "byte[]";
+                    break;
+                case "money":
+                    typeString = "decimal";
+                    break;
+                case "nchar":
+                    typeString = "string";
+                    break;
+                case "ntext":
+                    typeString = "string";
+                    break;
+                case "numeric":
+                    typeString = "decimal";
+                    break;
+                case "nvarchar":
+                    typeString = "string";
+                    break;
+                case "real":
+                    typeString = "System.Single";
+                    break;
+                case "smalldatetime":
+                    typeString = "DateTime";
+                    break;
+                case "smallint":
+                    typeString = "int";
+                    break;
+                case "smallmoney":
+                    typeString = "decimal";
+                    break;
+                case "timestamp":
+                    typeString = "DateTime";
+                    break;
+                case "tinyint":
+                    typeString = "byte";
+                    break;
+                case "uniqueidentifier":
+                    typeString = "System.Guid";
+                    break;
+                case "varbinary":
+                    typeString = "byte[]";
+                    break;
+                case "varchar":
+                    typeString = "string";
+                    break;
+                case "Variant":
+                    typeString = "object";
+                    break;
+                default:
+                    typeString = "object";
+                    break;
+            }
+            return typeString;
+        }
+    }
+
+}
